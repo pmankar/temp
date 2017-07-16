b681caa2bd2b0dada36d356e5c029c2fa00b4d9c
pmankar <pratik.m8@gmail.com>
2017-07-16 21:30:02 +0200

    contact us

--- wp-content/themes/accesspress-store-child/functions.php
+++ wp-content/themes/accesspress-store-child/functions.php
@@ -1,20 +1,23 @@
 <?php
 //* Code goes here
 
-/* 
-removes the qunatity from all products
-*/
+/* removes the qunatity from all products */
 function wc_remove_all_quantity_fields( $return, $product ) {
 	return true;
 }
 add_filter( 'woocommerce_is_sold_individually', 'wc_remove_all_quantity_fields', 10, 2 );
 
-
 /*
 rename additional information tab to 
-*/
 add_filter( 'woocommerce_product_tabs', 'woo_rename_tabs', 98 );
 function woo_rename_tabs( $tabs ) {
 	$tabs['additional_information']['title'] = __( 'Product Data' );	// Rename the additional information tab
 	return $tabs;
+}*/
+
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+function woo_remove_product_tabs( $tabs ) {
+    unset( $tabs['additional_information'] );  	// Remove the additional information tab
+    return $tabs;
 }
+
