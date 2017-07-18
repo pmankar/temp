6fe1891f9c424a5dc0297444e3d80c86be7ae5b5
pmankar <pratik.m8@gmail.com>
2017-07-19 01:38:41 +0200

    changes to single product page

--- wp-content/themes/accesspress-store-child/functions.php
+++ wp-content/themes/accesspress-store-child/functions.php
@@ -15,6 +15,7 @@ function woo_rename_tabs( $tabs ) {
 	return $tabs;
 }*/
 
+// remove additional information tab
 add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
 function woo_remove_product_tabs( $tabs ) {
     unset( $tabs['additional_information'] );  	// Remove the additional information tab
@@ -22,14 +23,51 @@ function woo_remove_product_tabs( $tabs ) {
 }
 
 
+// add add_to_cart on right sidebar on single product page
 add_filter('woocommerce_sidebar', 'woocommerce_addtocart_right', 10);
 function woocommerce_addtocart_right(){
-	woocommerce_template_single_price();
-	woocommerce_template_single_add_to_cart();
+	echo "<div id='product-";
+	echo the_ID();
+	echo "'";
+	echo post_class();
+	echo ">";
+		echo "<div class='summary entry-summary woocommerce content-area'>";
+			woocommerce_template_single_price();
+			woocommerce_template_single_add_to_cart();
+		echo "</div><!-- .summary -->";
+	echo"</div><!-- #product-<?php the_ID(); ?> -->";
 }
 
 
+// remove the add to cart from central area
 add_action('woocommerce_single_product_summary', 'woocommerce_remove_cart');
 function woocommerce_remove_cart(){
 	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
+}
+
+
+// remove existing meta info woocommerce_template_single_meta
+add_action('woocommerce_single_product_summary', 'woocommerce_remove_meta');
+function woocommerce_remove_meta(){
+	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+}
+
+
+// add more meta info:
+// id, date, country, pages, publisher, category
+add_filter('woocommerce_meta', 'add_more_meta', 40);
+function add_more_meta(){
+	woocommerce_template_single_title();
+	global $product;
+	$minfo = [];
+	$minfo[] = ( $sku = 'ID: '.$product->get_sku() ) ? $sku : __( 'N/A', 'accesspress-store' );
+	$minfo[] = array_shift(wc_get_product_terms( $product->id, 'pa_publishedon' ));
+	$minfo[] = array_shift(wc_get_product_terms( $product->id, 'pa_country' ));
+	$minfo[] = array_shift(wc_get_product_terms( $product->id, 'pa_format' ));
+	$minfo[] = array_shift(wc_get_product_terms( $product->id, 'pa_pages' ))." Pages";
+	$minfo[] = array_shift(wc_get_product_terms( $product->id, 'pa_publisher' ));
+	$minfo[] = $product->get_categories( ', ', '' . _n( 'Category:', 'Categories:', $cat_count, 'accesspress-store' ) . ' ', '' );
+	echo "<ul class='metainfo'>";
+	echo  "<li style='border:none;'>".implode( '</li><li>', $minfo)."</li>" ;
+	echo "</ul>";
 }
--- wp-content/themes/accesspress-store/woocommerce/content-single-product.php
+++ wp-content/themes/accesspress-store/woocommerce/content-single-product.php
@@ -63,8 +63,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 			 * @hooked woocommerce_template_single_sharing - 50
 			 * @hooked WC_Structured_Data::generate_product_data() - 60
 			 */
-			do_action( 'woocommerce_single_product_summary' );
-			
+			//do_action( 'woocommerce_single_product_summary' );
+			do_action('woocommerce_meta');
 		?>
 
 	</div><!-- .summary -->
